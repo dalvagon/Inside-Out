db1d59cc359e558ff0bd934281b02acadf439188
CapsaDragosGabriel <79151220+CapsaDragosGabriel@users.noreply.github.com>
2022-12-30 20:27:18 +0200

    added menu sound

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+ public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+        
+		Debug.Log ("You have clicked the button!");
+        Application.Quit();
+	}
+}
